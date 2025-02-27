3a107fc043d015cc6d63721b38620ac3ba0fedfa
IshaT-Hub <ishatiwari0508@gmail.com>
2025-02-27 18:21:27 +0530

    Creation of Custom Hook useCurrencyInfo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-black bg-fuchsia-400 font-bold">
+       Swap Penny 💱
+      </h1>
+    </>
+  );
+}
+
+export default App;
